5bc6790e43ac92ab0788f4146e7d46c3e65dbfd0
Cold <samedovvv00@gmail.com>
2022-12-17 19:12:47 +0400

    initial commit

---
+++ src/data/courses.js
@@ -0,0 +1,23 @@
+const courses = [
+  {
+    title: 'Complete React Guide',
+    slug: 'react',
+    id: 228,
+  },
+  {
+    title: 'Complete JavaScript Guide',
+    slug: 'javascript',
+    id: 333,
+  },
+  {
+    title: 'Complete Python Guide',
+    slug: 'python',
+    id: 123,
+  },
+  {
+    title: 'Complete Node.js Guide',
+    slug: 'node-js',
+    id: 322,
+  },
+]
+export default courses
